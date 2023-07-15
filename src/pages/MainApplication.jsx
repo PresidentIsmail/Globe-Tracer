615785366afd12920934b1868c46ae060ae44584
Ismail <shaikhnagismail@gmail.com>
2023-07-15 17:39:03 +0200

    when user goes to /app they are redirected to app/cities

--- src/pages/MainApplication.jsx
+++ src/pages/MainApplication.jsx
@@ -1,18 +1,24 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react"; // Don't forget to import useEffect
 import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
 import styles from "./MainApplication.module.css";
 
 const MainApplication = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  // when the component mounts navigate to the cities page
   useEffect(() => {
-    navigate("/app/cities");
-  }, [navigate]);
+     // Remove trailing slash if present from the current pathname
+     const trimmedPathname = location.pathname.replace(/\/$/, "");
 
+     // Check if the current pathname is exactly "/app"
+     if (trimmedPathname === "/app") {
+       // Redirect to "/app/cities" if the current pathname is "/app"
+       navigate("/app/cities");
+     }
+  }, [navigate, location]);
+ 
   return (
     <div className={styles.app}>
       <Sidebar />
